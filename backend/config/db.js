61bbd0bda72ac8a4bc1bbcd88ccabb63485aec6e
chandrakant kumar <chandrakantkumar1210@gmail.com>
2026-05-22 15:26:35 +0530

    Fixed analytics route

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("MongoDB Connected");
+  try {
+
+    await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log(
+      "MongoDB Connected Successfully"
+    );
+
   } catch (error) {
-    console.log(error);
+
+    console.log(
+      "MongoDB Connection Error:",
+      error
+    );
+
     process.exit(1);
   }
 };
